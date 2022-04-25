d22feb56408f641ffbcf2f46981e473f3bf7d3f9
Estellep-gif <estellepeney92@gmail.com>
2022-04-25 11:23:47 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - prints all possible different combinations of two digits
+  *
+  * Return: Always 0 (succeed)
+  */
+int main(void)
+{
+	int digi1, digi2;
+
+	for (digi1 = 0; digi1 < 10; digi1++)
+	{
+		for (digi2 = 0; digi2 < 10; digi2++)
+		{
+			putchar((digi1 % 10) + '0');
+			putchar((digi2 % 10 ) + '0');
+
+			if (digi1 == 9 && digi2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
