56f65ecb54a9e5e15dcd932a83205e0e56411c5d
Estellep-gif <estellepeney92@gmail.com>
2022-04-25 08:06:52 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - All the single digit number from 0 to 10
+  *
+  * Return: Always0 (succeed)
+  */
+int main(void)
+{
+	int a;
+	
+	for (a == 0; a <= 9; a++)
+	{
+		printf("%d",a);
+	}
+
+	printf("\n");
+	return (0);
+}
