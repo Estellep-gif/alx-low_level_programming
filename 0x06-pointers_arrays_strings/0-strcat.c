6279212a713a7e3f8a638e58bd9d502372ae1253
Estellep-gif <estellepeney92@gmail.com>
2022-04-26 16:10:02 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,24 +4,24 @@
   * @dest: Destination string                                                              
   * @src: the soure of the string                                                         
   * Return: dest
-  */                                                                        
-char *_strcat(char *dest, char *src)                                                  
-{                                                        
-	int i;                                                    
+  */
+char *_strcat(char *dest, char *src)
+{
+	int i;
 	int j;
 
 	i = 0;
 
-	while (dest[i] != '\0')                                                     
-	{                                                       
+	while (dest[i] != '\0')
+	{
 		i++;
-	}                                                           
+	}
 	j = 0;
 
 	while (src[j] != '\0')
 	{
-		dest[i] = src[j];                                                        
-		j++;                                                         
+		dest[i] = src[j];
+		j++;
 		i++;
 	}
 	dest[i] = '\0';
